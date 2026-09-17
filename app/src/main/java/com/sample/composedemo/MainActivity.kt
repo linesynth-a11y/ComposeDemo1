eb4b8a4177b7a68de0e663084bd93306f6c965a8
linesynth-a11y <linesynth@gmail.com>
2026-09-17 15:27:24 +0700

    DemoSlider

--- app/src/main/java/com/sample/composedemo/MainActivity.kt
+++ app/src/main/java/com/sample/composedemo/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sample.composedemo.ui.theme.ComposeDemoTheme
 
@@ -44,4 +47,13 @@ fun DemoTextPreview() {
     ComposeDemoTheme {
         DemoText(message = "Welcome to Android", fontSize = 12f)
     }
+}
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit ) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = onPositionChange
+    )
 }
